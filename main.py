fc293db6a28790aaabe8bdff2549201f99722901
vignesh <vignesh2914@gmail.com>
2025-01-09 00:05:06 +0530

    Main updated

--- main.py
+++ main.py
@@ -5,30 +5,28 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_groq import ChatGroq
 
-def configure():
-    load_dotenv()
 
-load_dotenv()
-LANGCHAIN_API_KEY=os.getenv("LANGCHAIN_API_KEY")
-# LANGCHAIN_PROJECT=os.getenv("LANGCHAIN_PROJECT")
-GROQ_API_KEY=os.getenv("GROQ_API_KEY")
+# Access API keys from Streamlit secrets
+GROQ_API_KEY = st.secrets["GROQ_API_KEY"]["value"]
+LANGCHAIN_API_KEY = st.secrets["LANGCHAIN_API_KEY"]["value"]
 
-os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
 
-prompt_template=ChatPromptTemplate.from_messages(
+# Create the prompt template
+prompt_template = ChatPromptTemplate.from_messages(
     [
-        ("system","You are a helpful assistant. Please respond to the question asked"),
-        ("user","Question:{question}")
+        ("system", "You are a helpful assistant. Please respond to the question asked"),
+        ("user", "Question:{question}")
     ]
 )
 
 st.set_page_config(page_title="Conversational AI chatbot", page_icon="🦜")
 st.title(body="Conversational AI chatbot")
-input_text=st.text_input(label="What question you have in mind?")
+input_text = st.text_input(label="What question you have in mind?")
 
-llm=ChatGroq(api_key=GROQ_API_KEY,model_name="Llama3-8b-8192")
-output_parser=StrOutputParser()
-chain=prompt_template|llm|output_parser
+llm = ChatGroq(api_key=GROQ_API_KEY, model_name="Llama3-8b-8192")
+output_parser = StrOutputParser()
+chain = prompt_template | llm | output_parser
 
 if input_text:
-    st.write(chain.invoke({"question":input_text}))
+    st.write(chain.invoke({"question": input_text}))
