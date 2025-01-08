586ae8cdff8bd731f9e2d810aed6a06a8f74d3de
vignesh <vignesh2914@gmail.com>
2025-01-09 00:07:03 +0530

    Main updated now we can use local as well streamlit cloud

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_groq import ChatGroq
 
+load_dotenv()
 
-# Access API keys from Streamlit secrets
-GROQ_API_KEY = st.secrets["GROQ_API_KEY"]["value"]
-LANGCHAIN_API_KEY = st.secrets["LANGCHAIN_API_KEY"]["value"]
+GROQ_API_KEY = os.getenv("GROQ_API_KEY") if "GROQ_API_KEY" in os.environ else st.secrets["GROQ_API_KEY"]["value"]
+LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY") if "LANGCHAIN_API_KEY" in os.environ else st.secrets["LANGCHAIN_API_KEY"]["value"]
 
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 
@@ -20,13 +20,16 @@ prompt_template = ChatPromptTemplate.from_messages(
     ]
 )
 
+# Streamlit UI
 st.set_page_config(page_title="Conversational AI chatbot", page_icon="🦜")
 st.title(body="Conversational AI chatbot")
 input_text = st.text_input(label="What question you have in mind?")
 
+# Set up the LLM
 llm = ChatGroq(api_key=GROQ_API_KEY, model_name="Llama3-8b-8192")
 output_parser = StrOutputParser()
 chain = prompt_template | llm | output_parser
 
+# Process user input
 if input_text:
     st.write(chain.invoke({"question": input_text}))
